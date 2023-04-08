ef23f2cafa689ed6ee81a946c903625f94d738f7
Danny Fong <danny.fong.dev@outlook.com>
2023-04-07 18:31:25 -0700

    Border left has primary color of job is new

--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -1,6 +1,10 @@
 <template>
   <div
-    class="mx-auto mb-10 flex rounded-md bg-white p-6 shadow-lg md:mb-5 md:max-w-[1110px]"
+    :class="
+      job.new
+        ? 'mx-auto mb-10 flex rounded-md border-l-4 border-primary1 bg-white p-6 shadow-lg md:mb-5 md:max-w-[1110px]'
+        : 'mx-auto mb-10 flex rounded-md bg-white p-6 shadow-lg md:mb-5 md:max-w-[1110px]'
+    "
   >
     <Job :job="job" @filter="handleFilter" />
   </div>
