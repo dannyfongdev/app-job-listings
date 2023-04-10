b7fec11370013097cce462e054979698d0463da4
Danny Fong <danny.fong.dev@outlook.com>
2023-04-10 15:19:41 -0700

    Filter component moble css

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,41 +1,64 @@
 <template>
-  <div v-if="hasFilters" class="flex justify-between bg-neutral1 p-6 md:mb-6">
-    <ul class="flex gap-4">
-      <li v-if="role">
-        {{ role
-        }}<span
-          class="cursor-pointer p-1"
-          v-html="CROSS"
-          @click="emit('clear', 'role')"
-        ></span>
-      </li>
-      <li v-if="level">
-        {{ level
-        }}<span
-          class="cursor-pointer p-1"
-          v-html="CROSS"
-          @click="emit('clear', 'level')"
-        ></span>
-      </li>
-      <li v-for="language in languages" :key="language">
-        {{ language
-        }}<span
-          class="cursor-pointer p-1"
-          v-html="CROSS"
-          @click="emit('clear', 'language', language)"
-        ></span>
-      </li>
-      <li v-for="tool in tools" :key="tool">
-        {{ tool
-        }}<span
-          class="cursor-pointer p-1"
-          v-html="CROSS"
-          @click="emit('clear', 'tool', tool)"
-        ></span>
-      </li>
-    </ul>
-    <div class="cursor-pointer hover:underline" @click="emit('clear', 'all')">
-      Clear
+  <div v-if="hasFilters" class="bg-neutral1 p-6 shadow-lg">
+    <div
+      class="mx-auto flex items-center justify-between rounded-md bg-white p-6 md:max-w-[1110px]"
+    >
+      <ul class="flex flex-wrap gap-4">
+        <li v-if="role" class="flex items-center rounded-l-md bg-neutral1">
+          <div class="px-2.5 font-bold text-primary1">{{ role }}</div>
+          <div
+            class="cursor-pointer rounded-r-md bg-primary1 p-1.5 text-white hover:bg-black"
+            v-html="CROSS"
+            @click="emit('clear', 'role')"
+          ></div>
+        </li>
+
+        <li
+          v-if="level"
+          class="flex items-center rounded-l-md bg-neutral1 font-bold text-primary1"
+        >
+          <div class="px-2.5">{{ level }}</div>
+
+          <div
+            class="cursor-pointer rounded-r-md bg-primary1 p-1.5 text-white hover:bg-black"
+            v-html="CROSS"
+            @click="emit('clear', 'level')"
+          ></div>
+        </li>
+        <li
+          v-for="language in languages"
+          :key="language"
+          class="flex items-center rounded-md bg-neutral1 font-bold text-primary1"
+        >
+          <div class="px-2.5">{{ language }}</div>
+          <div
+            class="cursor-pointer rounded-r-md bg-primary1 p-1.5 text-white hover:bg-black"
+            v-html="CROSS"
+            @click="emit('clear', 'language', language)"
+          ></div>
+        </li>
+
+        <li
+          v-for="tool in tools"
+          :key="tool"
+          class="flex items-center rounded-l-md bg-neutral1 font-bold text-primary1"
+        >
+          <div class="px-2.5">
+            {{ tool }}
+          </div>
+          <div
+            class="cursor-pointer rounded-r-md bg-primary1 p-1.5 text-white hover:bg-black"
+            v-html="CROSS"
+            @click="emit('clear', 'tool', tool)"
+          ></div>
+        </li>
+      </ul>
+      <div
+        class="inline-block cursor-pointer rounded-md py-1.5 px-3 font-bold text-neutral3 hover:text-primary1 hover:underline md:h-8"
+        @click="emit('clear', 'all')"
+      >
+        Clear
+      </div>
     </div>
   </div>
 </template>
